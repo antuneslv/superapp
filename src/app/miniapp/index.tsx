044a596683342202df6fd6cac0210b8720623c93
Leandro Vendemiatto Antunes <antuneslv@hotmail.com>
2024-10-21 09:26:58 -0300

    feat: send variables to miniapp

--- src/app/miniapp/index.tsx
+++ src/app/miniapp/index.tsx
@@ -4,6 +4,29 @@ import { WebView, WebViewNavigation } from 'react-native-webview'
 import { TopBar } from '../components/TopBar'
 import { useRouter } from 'expo-router'
 
+const html = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Miniapp</h1>
+  <p id="token-id"></p>
+  <script>
+    const customValue = JSON.parse(window.ReactNativeWebView.injectedObjectJson()).myApp
+   
+    
+    const pId = document.getElementById("token-id")
+    pId.innerHTML = customValue.token
+    window.ReactNativeWebView.postMessage(JSON.stringify(customValue))
+  </script>
+</body>
+</html>
+`
+
 export default function Miniapp() {
   const webViewRef = useRef<WebView>(null)
   const [canGoBack, setCanGoBack] = useState(false)
@@ -54,7 +77,16 @@ export default function Miniapp() {
       <WebView
         ref={webViewRef}
         onNavigationStateChange={handleNavigationStateChange}
-        source={{ uri: 'https://www.google.com/' }}
+        injectedJavaScriptObject={{
+          myApp: {
+            token: '1234',
+            user: 'John Doe'
+          }
+        }}
+        onMessage={event => {
+          console.log(event.nativeEvent.data)
+        }}
+        source={{ html }}
       />
     </View>
   )
